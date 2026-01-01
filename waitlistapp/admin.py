ea59c7e2eeb976f3f15c6e6d7cdacf970bbb877e
Ofeh-Mamuzoh Elozino <eofeh-mamuzoh.2301630@stu.cu.edu.ng>
2026-01-02 03:31:51 +0530

    Update admin.py

--- waitlistapp/admin.py
+++ waitlistapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import WaitlistSignup
 
-# This allows you to see the signups in the Django Admin panel
-admin.site.register(WaitlistSignup)
+@admin.register(WaitlistSignup)
+class WaitlistSignupAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'location', 'farming_type') # Adds the column to the dashboard
