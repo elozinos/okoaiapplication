db232b17062e9556b3b2531001e563ea4fbbb21c
Ofeh-Mamuzoh Elozino <eofeh-mamuzoh.2301630@stu.cu.edu.ng>
2026-01-02 02:48:35 +0530

    Add farming_type field to WaitlistSignup model
    Added a new field 'farming_type' with choices for farming methods.

--- waitlistapp/models.py
+++ waitlistapp/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-# Create your models here.
-
-
-
 class WaitlistSignup(models.Model):
+    # Your existing fields
     full_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
     location = models.CharField(max_length=255)
     farm_size = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.full_name
+    # NEW FIELD
+    FARMING_CHOICES = [
+        ('crop', 'Crop Farming'),
+        ('livestock', 'Livestock Farming'),
+        ('mixed', 'Mixed Farming'),
+        ('other', 'Other'),
+    ]
+    farming_type = models.CharField(
+        max_length=50, 
+        choices=FARMING_CHOICES, 
+        default='crop'
+    )
